680943358c954769f53ef681b475a5dd71ea7869
Bumbi <annika.borgesson@gmail.com>
2024-02-21 12:40:17 +0100

    Add secret keys

--- posterblog/settings.py
+++ posterblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3^a$4g090^s=d)kjy=y!wqnsctgsd(^^qxvn89#4wh1y8t7_r+"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
