e0cdfb005c25d3c34bdc1164f401983f3bad2d37
Bumbi <annika.borgesson@gmail.com>
2024-02-21 14:21:12 +0100

    Add blog.html, try fix urls paths. add summernote

--- posterblog/urls.py
+++ posterblog/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import my_blog
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
-    path('blog/', my_blog, name='blog'),
+    path("blog/", include("blog.urls"), name="blog-urls"),
+    path('summernote/', include('django_summernote.urls')),
 ]
