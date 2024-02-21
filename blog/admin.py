b2f58927609667e279952e7768a345d98fb61e6d
Bumbi <annika.borgesson@gmail.com>
2024-02-21 14:42:27 +0100

    Fix typo in postadmin for summernote to work. Redid change in models.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Category, Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('post_content',)
+
+
 admin.site.register(Category)
-admin.site.register(Post)
 admin.site.register(Comment)
+
