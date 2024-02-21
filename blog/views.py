f53cc13341af45887be9b71f9ae3bdd6f9f71bd6
Bumbi <annika.borgesson@gmail.com>
2024-02-21 22:43:00 +0100

    Add blog.html, add basic structure to it so it will show blog posts

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,5 @@ from .models import Post
 
 class PostList(generic.ListView):
      queryset = Post.objects.filter(status=1)
-     template_name = "blog.html"
+     template_name = "blog/blog.html"
+     paginate_by = 6
