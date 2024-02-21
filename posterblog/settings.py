e04b34b649ac545f9a70480e3e632eca7b475a9f
Bumbi <annika.borgesson@gmail.com>
2024-02-21 12:26:48 +0100

    made  code ready for deploy

--- posterblog/settings.py
+++ posterblog/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = "django-insecure-3^a$4g090^s=d)kjy=y!wqnsctgsd(^^qxvn89#4wh1y8t7_r+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  "127.0.0.1",
+  ".herokuapp.com",
+  ]
 
 
 # Application definition
