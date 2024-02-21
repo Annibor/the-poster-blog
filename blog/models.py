acbbcce47ac1c915a945fa56d87f9ce9260ad154
Bumbi <annika.borgesson@gmail.com>
2024-02-21 13:02:49 +0100

    Add Category & Post model to blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+   """
+      A model representing a category for organizing blog posts.
+
+    Attributes:
+        name: The name of the category.
+    """
+   name = models.CharField(max_length=40)
+
+   class Meta:
+        verbose_name_plural = "categories"
+        
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+
+class Post(models.Model):
+  """
+    This model represents a post.
+
+    Attributes:
+    title: The title of the post.
+    slug: The slug of the post.
+    post_content: The content of the post.
+    created_on: The date and time when the post was created.
+    last_modifed: The last time the post was updated or changed.
+    categoris: The categoris associated with the post.
+    """
+  title = models.CharField(max_length=250, unique=True)
+  slug = models.SlugField(max_length=250, unique=True)
+  post_content = models.TextField()
+  created_on = models.DateTimeField(auto_now_add=True)
+  last_modified = models.DateTimeField(auto_now=True)
+  categories = models.ManyToManyField("Category", related_name="posts")
+  status = models.IntegerField(choices=STATUS, default=0)
+
+  def __str__(self):
+    return self.title
+
+  class Meta:
+    """
+    Blog Meta Data
+    """
+    ordering = ["-created_on"]
+    verbose_name_plural = "Posts"
+
+
+  def __str__(self):
+       return f"{self.title}"
