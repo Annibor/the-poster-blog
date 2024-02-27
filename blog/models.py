14587bfd9e84197846988b5a2c9f79aa52f47754
Bumbi <annika.borgesson@gmail.com>
2024-02-27 13:00:11 +0100

    add cludinary to store img, added images to post model #17

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 class Category(models.Model):
    """
@@ -33,6 +35,7 @@ class Post(models.Model):
     """
   title = models.CharField(max_length=250, unique=True)
   slug = models.SlugField(max_length=250, unique=True)
+  featured_image = CloudinaryField('image', default='placeholder')
   post_content = models.TextField()
   created_on = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
