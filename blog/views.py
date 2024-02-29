21ac2b8957209dc40533466820bdd97020284513
Bumbi <annika.borgesson@gmail.com>
2024-02-29 20:54:23 +0100

    Add Postdetail class -> get_conext_data, -> make comments work. import DetailView #17 #8

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib import messages
 from django.db.models import Count, Q
+from django.views.generic.detail import DetailView
 from .models import Post
 from .forms import CommentForm
 
@@ -19,23 +20,45 @@ class PostList(generic.ListView):
      paginate_by = 6
 
      def get_queryset(self):
-        """
-        Overrides the default queryset to filter posts by status, annotate each post with
-        the count of approved comments, and order the posts by their creation date in descending order.
-        """
-        return Post.objects.filter(status=1).annotate(
-            approved_comments_count=Count('comments', filter=Q(comments__approved=True))
-        ).order_by('-created_on')
+         """
+         Overrides the default queryset to filter posts by status, annotate each post with
+         the count of approved comments, and order the posts by their creation date in descending order.
+         """
+         return Post.objects.filter(status=1).annotate(
+             approved_comments_count=Count('comments', filter=Q(comments__approved=True))
+         ).order_by('-created_on')
      
      def get_context_data(self, **kwargs):
-        """
-        Extends the base implementation to add the list of posts to the context.
-        """
-        context = super().get_context_data(**kwargs)
-        queryset = self.get_queryset()
-        posts = list(queryset)
-        context['posts'] = posts
-        return context
+         """
+         Extends the base implementation to add the list of posts to the context.
+         """
+         context = super().get_context_data(**kwargs)
+         queryset = self.get_queryset()
+         posts = list(queryset)
+         context['posts'] = posts
+         return context
+     
+
+class PostDetail(DetailView):
+    """
+    A view that inherits from Django's DetailView for displaying a single post detail.
+    It includes comments and a form for adding new comments.
+    """
+    model = Post
+    template_name = "blog/blog.html"
+
+    def get_context_data(self, **kwargs):
+         """
+         Overrides to add comments, comment form, and comment count to the context for the post.
+         """
+         context = super().get_context_data(**kwargs)
+         post = context['post']
+         comments = post.comments.filter(approved=True).order_by('-created_on')
+         context['comments'] = comments
+         context['comment_form'] = CommentForm()
+         context['comment_count'] = comments.count()
+         return context
+    
 
 
 def comment_content(request, slug):
