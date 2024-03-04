6fe0443474cc85c242798a0bd4518955a3219267
Bumbi <annika.borgesson@gmail.com>
2024-03-04 20:16:05 +0100

    Select elements from html in  script.js, check they work #26 #17

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+document.addEventListener("DOMContentLoaded", function() {
+    const editButtons = document.querySelectorAll(".edit-comment-btn"); 
+    const commentText = document.querySelector("#bodyField"); 
+    const commentForm = document.querySelector("#commentForm"); 
+    const submitButton = document.querySelector("#submitButton");
+
+    console.log("editButton:", editButtons);
+    console.log("commentText:", commentText);
+    console.log("commentForm:", commentForm);
+    console.log("submitButton:", submitButton);
+    console.log("Edit buttons found: ", editButtons.length);
+});
