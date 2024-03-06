658b0dcfc51190227f50dbbfdcfc1375ffe542c5
Bumbi <annika.borgesson@gmail.com>
2024-03-06 09:29:20 +0100

    Delete unused imports code in views

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
 from django.http import JsonResponse
 from django.views.generic import UpdateView
-from django.urls import reverse_lazy
 from django.views import View
 from django.views import generic
 from django.contrib import messages
