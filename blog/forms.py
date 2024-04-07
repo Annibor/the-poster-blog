30665b76aa537d3f8e408a0bbf8638fbd9b27a7f
Bumbi <annika.borgesson@gmail.com>
2024-04-07 21:01:40 +0200

    Made sigup form work correctly #34 #32
    Found tutorial on how to overwrite allauth template,
     so I changed that

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,4 @@
+from allauth.account.forms import SignupForm
 from .models import Comment
 from django import forms
 
@@ -10,3 +11,28 @@ class CommentForm(forms.ModelForm):
             'body': forms.Textarea(attrs={'class': 'form-control',
                                           'placeholder': 'Leave a comment'}),
         }
+
+class RegisterForm(SignupForm):
+    """
+    Adapted Form for custom signup page from
+    https://docs.allauth.org/en/latest/account/forms.html
+    """
+    first_name = forms.CharField(
+        max_length=45,
+        label="First Name",
+        widget=forms.TextInput(attrs={"placeholder": "First Name"}),
+    )
+    username = forms.CharField(
+        max_length=45,
+        label="Username",
+        widget=forms.TextInput(attrs={"placeholder": "Username"}),
+    )
+    
+    def register(self, request, user):
+        # Changes allauth form to costume form
+        user = super(RegisterForm, self).save(request)
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.username = self.cleaned_data["username"]
+        user.save()
+        return user
